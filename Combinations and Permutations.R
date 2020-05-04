066b2fbd49fa425456fd5981729331f0566a8a88
Michelle Ruas <michelle.ruas@gmail.com>
2020-05-04 20:57:02 +0100

    Combinations and Permutations

---
+++ Combinations and Permutations.R
@@ -0,0 +1,171 @@
+# joining strings with paste
+number <- "Three"
+suit <- "Hearts"
+paste(number, suit)
+
+# joining vectors element-wise with paste
+paste(letters[1:5], as.character(1:5))
+
+# generating combinations of 2 vectors with expand.grid
+expand.grid(pants = c("blue", "black"), shirt = c("white", "grey", "plaid"))
+
+suits <- c("Diamonds", "Clubs", "Hearts", "Spades")
+numbers <- c("Ace", "Deuce", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King")
+deck <- expand.grid(number = numbers, suit = suits)
+deck <- paste(deck$number, deck$suit)
+
+# probability of drawing a king
+kings <- paste("King", suits)
+mean(deck %in% kings)
+
+library(gtools)
+permutations(5,2)    # ways to choose 2 numbers in order from 1:5
+all_phone_numbers <- permutations(10, 7, v = 0:9)
+n <- nrow(all_phone_numbers)
+index <- sample(n, 5)
+all_phone_numbers[index,]
+
+permutations(3,2)    # order matters
+combinations(3,2)    # order does not matter
+
+hands <- permutations(52,2, v = deck)
+first_card <- hands[,1]
+second_card <- hands[,2]
+sum(first_card %in% kings)
+
+sum(first_card %in% kings & second_card %in% kings) / sum(first_card %in% kings)
+
+aces <- paste("Ace", suits)
+facecard <- c("King", "Queen", "Jack", "Ten")
+facecard <- expand.grid(number = facecard, suit = suits)
+facecard <- paste(facecard$number, facecard$suit)
+
+hands <- combinations(52, 2, v=deck) # all possible hands
+
+# probability of a natural 21 given that the ace is listed first in `combinations`
+mean(hands[,1] %in% aces & hands[,2] %in% facecard)
+
+# probability of a natural 21 checking for both ace first and ace second
+mean((hands[,1] %in% aces & hands[,2] %in% facecard)|(hands[,2] %in% aces & hands[,1] %in% facecard))
+
+# code for one hand of blackjack
+hand <- sample(deck, 2)
+hand
+
+# code for B=10,000 hands of blackjack
+B <- 10000
+results <- replicate(B, {
+  hand <- sample(deck, 2)
+  (hand[1] %in% aces & hand[2] %in% facecard) | (hand[2] %in% aces & hand[1] %in% facecard)
+})
+mean(results)
+
+# checking for duplicated bdays in one 50 person group
+n <- 50
+bdays <- sample(1:365, n, replace = TRUE)    # generate n random birthdays
+bdays
+any(duplicated(bdays))    # check if any birthdays are duplicated
+
+
+# Monte Carlo simulation with B=10000 replicates
+B <- 10000
+results <- replicate(B, {    # returns vector of B logical values
+  bdays <- sample(1:365, n, replace = TRUE)
+  any(duplicated(bdays))
+})
+mean(results)    # calculates proportion of groups with duplicated bdays
+
+
+# function to calculate probability of shared bdays across n people
+compute_prob <- function(n, B = 10000) {
+  same_day <- replicate(B, {
+    bdays <- sample(1:365, n, replace = TRUE)
+    any(duplicated(bdays))
+  })
+  mean(same_day)
+}
+
+n <- seq(1, 60)
+
+x <- 1:10
+sqrt(x)    # sqrt operates on each element of the vector
+
+y <- 1:10
+x*y    # * operates element-wise on both vectors
+
+compute_prob(n)    # does not iterate over the vector n without sapply
+
+x <- 1:10
+sapply(x, sqrt)    # this is equivalent to sqrt(x)
+
+prob <- sapply(n, compute_prob)    # element-wise application of compute_prob to n
+plot(n, prob)
+
+
+# function for computing exact probability of shared birthdays for any n
+exact_prob <- function(n){
+  prob_unique <- seq(365, 365-n+1)/365   # vector of fractions for mult. rule
+  1 - prod(prob_unique)    # calculate prob of no shared birthdays and subtract from 1
+}
+
+# applying function element-wise to vector of n values
+eprob <- sapply(n, exact_prob)
+
+# plotting Monte Carlo results and exact probabilities on same graph
+plot(n, prob)    # plot Monte Carlo results
+lines(n, eprob, col = "red")    # add line for exact prob
+
+B <- 10^seq(1, 5, len = 100)    # defines vector of many B values
+compute_prob <- function(B, n = 22){    # function to run Monte Carlo simulation with each B
+  same_day <- replicate(B, {
+    bdays <- sample(1:365, n, replace = TRUE)
+    any(duplicated(bdays))
+  })
+  mean(same_day)
+}
+
+prob <- sapply(B, compute_prob)    # apply compute_prob to many values of B
+plot(log10(B), prob, type = "l")    # plot a line graph of estimates 
+
+# Assign the variable 'p_yellow' as the probability that a yellow ball is drawn from the box.
+p_yellow <- yellow/(cyan+magenta+yellow)
+
+# Using the variable 'p_yellow', calculate the probability of drawing a yellow ball on the sixth draw. Print this value to the console.
+p_yellow
+
+# Assign the variable 'p_no6' as the probability of not seeing a 6 on a single roll.
+p_no6 <- 5/6
+
+
+# Calculate the probability of not seeing a 6 on six rolls using `p_no6`. Print your result to the console: do not assign it to a variable.
+p_no6*p_no6*p_no6*p_no6*p_no6*p_no6
+
+# Assign the variable `p_cavs_win4` as the probability that the Cavs will win the first four games of the series.
+p_cavs_win4 <- 0.6^4
+
+
+# Using the variable `p_cavs_win4`, calculate the probability that the Celtics win at least one game in the first four games of the series.
+1-p_cavs_win4
+
+
+# This line of example code simulates four independent random games where the Celtics either lose or win. Copy this example code to use within the `replicate` function.
+simulated_games <- sample(c("lose","win"), 4, replace = TRUE, prob = c(0.6, 0.4))
+
+# The variable 'B' specifies the number of times we want the simulation to run. Let's run the Monte Carlo simulation 10,000 times.
+B <- 10000
+
+# Use the `set.seed` function to make sure your answer matches the expected result after random sampling.
+set.seed(1)
+
+# Create an object called `celtic_wins` that replicates two steps for B iterations: (1) generating a random four-game series `simulated_games` using the example code, then (2) determining whether the simulated series contains at least one win for the Celtics.
+celtic_wins <- replicate(B, {
+  simulated_games <- sample(c("lose","win"), 4, replace = TRUE, prob = c(0.6, 0.4)) 
+  "win" %in% simulated_games
+})
+
+
+# Calculate the frequency out of B iterations that the Celtics won at least one game. Print your answer to the console.
+mean(celtic_wins)
+
+
+
--- Monte Carlo Simulation.R
+++ Monte Carlo Simulation.R
@@ -19,4 +19,44 @@ beads
 mean(beads == "blue")
 # 0.6
 
-s
+#One ball will be drawn at random from a box containing: 3 cyan balls, 5 magenta balls, and 7 yellow balls.
+beads2 <- rep(c("cyan", "magenta", "yellow"), times = c(3,5,7))
+B <- 10000    # number of times to draw 1 bead
+events2 <- replicate(B, sample(beads2, 1))    # draw 1 bead, B times
+tab2 <- table(events2)    # make a table of outcome counts
+mean(beads2 == "cyan")
+
+beads3 <- rep(c("cyan", "magenta", "yellow"), times = c(2,5,7))
+0.2 * (1-mean(beads2 == "cyan"))
+
+
+cyan <- 3
+magenta <- 5
+yellow <- 7
+
+# Assign a variable `p` as the probability of choosing a cyan ball from the box
+p <- cyan/15
+
+# Print the variable `p` to the console
+p
+
+# `p` is defined as the probability of choosing a cyan ball from a box containing: 3 cyan balls, 5 magenta balls, and 7 yellow balls.
+# Using variable `p`, calculate the probability of choosing any ball that is not cyan from the box
+1-p
+
+# The variable `p_1` is the probability of choosing a cyan ball from the box on the first draw.
+p_1 <- cyan / (cyan + magenta + yellow)
+
+# Assign a variable `p_2` as the probability of not choosing a cyan ball on the second draw without replacement.
+p_2 <- 1- ((cyan-1)/(cyan-1+magenta+yellow))
+
+# Calculate the probability that the first draw is cyan and the second draw is not cyan using `p_1` and `p_2`.
+p_1 * p_2
+
+# Assign a variable 'p_2' as the probability of not choosing a cyan ball on the second draw with replacement.
+p_2 <- 1 -( cyan/ (cyan + magenta + yellow))
+
+# Calculate the probability that the first draw is cyan and the second draw is not cyan using `p_1` and `p_2`.
+p_1*p_2
+
+
