8d57e2983270df490333d39f3127e90d0dd43862
Michelle Ruas <michelle.ruas@gmail.com>
2020-05-05 00:12:45 +0100

    Monte Carlo Simulation Assessment

--- Combinations and Permutations.R
+++ Combinations and Permutations.R
@@ -12,6 +12,7 @@ expand.grid(pants = c("blue", "black"), shirt = c("white", "grey", "plaid"))
 suits <- c("Diamonds", "Clubs", "Hearts", "Spades")
 numbers <- c("Ace", "Deuce", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King")
 deck <- expand.grid(number = numbers, suit = suits)
+deck
 deck <- paste(deck$number, deck$suit)
 
 # probability of drawing a king
@@ -165,6 +166,84 @@ celtic_wins <- replicate(B, {
 
 # Calculate the frequency out of B iterations that the Celtics won at least one game. Print your answer to the console.
 mean(celtic_wins)
+
+
+# Assign a variable 'n' as the number of remaining games.
+n <- 7
+
+# Assign a variable `outcomes` as a vector of possible game outcomes, where 0 indicates a loss and 1 indicates a win for the Cavs.
+outcomes <- c(0,1)
+outcomes
+
+# Assign a variable `l` to a list of all possible outcomes in all remaining games. Use the `rep` function on `list(outcomes)` to create list of length `n`.
+l <- sample(outcomes, n, replace = TRUE, prob = c(0.5, 0.5))
+
+
+# Create a data frame named 'possibilities' that contains all combinations of possible outcomes for the remaining games.
+possibilities <- expand.grid(game1 = outcomes,game2 = outcomes, game3 = outcomes, game4= outcomes, game5=outcomes, game6=outcomes )
+
+
+# Create a vector named 'results' that indicates whether each row in the data frame 'possibilities' contains enough wins for the Cavs to win the series.
+results <- rowSums(possibilities)
+
+
+
+# Calculate the proportion of 'results' in which the Cavs win the series. Print the outcome to the console.
+mean(results >=4)
+
+
+# The variable `B` specifies the number of times we want the simulation to run. Let's run the Monte Carlo simulation 10,000 times.
+B <- 10000
+
+# Use the `set.seed` function to make sure your answer matches the expected result after random sampling.
+set.seed(1)
+
+# Create an object called `results` that replicates for `B` iterations a simulated series and determines whether that series contains at least four wins for the Cavs.
+results <- replicate(B, {
+  games <- sample(c(0,1), 6, replace = TRUE)
+  sum(games)
+})
+results
+# Calculate the frequency out of `B` iterations that the Cavs won at least four games in the remainder of the series. Print your answer to the console.
+mean(results >=4)
+
+# Let's assign the variable 'p' as the vector of probabilities that team A will win.
+p <- seq(0.5, 0.95, 0.025)
+
+# Given a value 'p', the probability of winning the series for the underdog team B can be computed with the following function based on a Monte Carlo simulation:
+prob_win <- function(p){
+  B <- 10000
+  result <- replicate(B, {
+    b_win <- sample(c(1,0), 7, replace = TRUE, prob = c(1-p, p))
+    sum(b_win)>=4
+  })
+  mean(result)
+}
+
+# Apply the 'prob_win' function across the vector of probabilities that team A will win to determine the probability that team B will win. Call this object 'Pr'.
+Pr <- sapply(p, prob_win)
+
+# Plot the probability 'p' on the x-axis and 'Pr' on the y-axis.
+plot(p, Pr)
+
+# Given a value 'p', the probability of winning the series for the underdog team B can be computed with the following function based on a Monte Carlo simulation:
+prob_win <- function(N, p=0.75){
+  B <- 10000
+  result <- replicate(B, {
+    b_win <- sample(c(1,0), N, replace = TRUE, prob = c(1-p, p))
+    sum(b_win)>=(N+1)/2
+  })
+  mean(result)
+}
+
+# Assign the variable 'N' as the vector of series lengths. Use only odd numbers ranging from 1 to 25 games.
+N <- seq(1,25,2)
+
+# Apply the 'prob_win' function across the vector of series lengths to determine the probability that team B will win. Call this object `Pr`.
+Pr <- sapply(N, prob_win)
+
+# Plot the number of games in the series 'N' on the x-axis and 'Pr' on the y-axis.
+plot(N, Pr)
 
 
 
---
+++ Olympic Running.R
@@ -0,0 +1,24 @@
+library(gtools)
+library(tidyverse)
+
+suits <- c("Gold", "Silver", "Bronze")
+numbers <- c("Bolt", "Blake", "Weir", "Four", "Five", "Six", "Seven", "Eight")
+deck <- expand.grid(number = numbers, suit = suits)
+deck <- paste(deck$number, deck$suit)
+
+coms <- combinations(8,3)
+perms <- permutations(3,3)
+coms
+str(coms)
+mean(coms)
+
+1/8^3*6
+
+runners <- c("Jamaica", "Jamaica", "Jamaica", "USA", "Ecuador", "Netherlands", "France", "South Africa")
+medals <- c("Gold","Silver","Bronze")
+
+B <- 10000
+results <- replicate(B, {
+  winners <- sample(runners, 3)
+})
+winners
