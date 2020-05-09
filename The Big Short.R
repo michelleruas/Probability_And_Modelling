f808f12a17a36270295e5821bf329e8c3c1d6c22
Michelle Ruas <michelle.ruas@gmail.com>
2020-05-09 22:45:50 +0100

    The big short scenarios

--- The Big Short.R
+++ The Big Short.R
@@ -41,4 +41,41 @@ mean(profit)    # expected value of the profit over n loans
 mean(profit<0)    # probability of losing money
 
 
+p <- .04
+loss_per_foreclosure <- -200000
+r <- 0.05
+x <- r*180000
+loss_per_foreclosure*p + x*(1-p)
+
+z <- qnorm(0.01)
+l <- loss_per_foreclosure
+n <- ceiling((z^2*(x-l)^2*p*(1-p))/(l*p + x*(1-p))^2)
+n    # number of loans required
+n*(loss_per_foreclosure*p + x * (1-p))    # expected profit over n loans
+B <- 10000
+p <- 0.04
+x <- 0.05 * 180000
+profit <- replicate(B, {
+  draws <- sample( c(x, loss_per_foreclosure), n, 
+                   prob=c(1-p, p), replace = TRUE) 
+  sum(draws)
+})
+mean(profit)
+
+p <- 0.04
+x <- 0.05*180000
+profit <- replicate(B, {
+  new_p <- 0.04 + sample(seq(-0.01, 0.01, length = 100), 1)
+  draws <- sample( c(x, loss_per_foreclosure), n, 
+                   prob=c(1-new_p, new_p), replace = TRUE)
+  sum(draws)
+})
+mean(profit)    # expected profit
+mean(profit < 0)    # probability of losing money
+mean(profit < -10000000)    # probability of losing over $10 million
+
+
+
+
+
 
