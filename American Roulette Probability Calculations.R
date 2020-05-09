d8b1dbfda1ef7f44aacb9e96565f5eeeccb1a5e5
Michelle Ruas <michelle.ruas@gmail.com>
2020-05-09 11:20:36 +0100

    American Roulette monte carlo simulations

--- American Roulette Probability Calculations.R
+++ American Roulette Probability Calculations.R
@@ -48,4 +48,66 @@ p_not_green <- 1-p_green
 # Calculate the expected outcome if you win $17 if the ball lands on green and you lose $1 if the ball doesn't land on green
 (p_green/(p_green+p_not_green))*17+(p_not_green/(p_green+p_not_green))*-1
 
+# Assign a variable `p_green` as the probability of the ball landing in a green pocket
+p_green <- green / (green+black+red)
+
+# Assign a variable `p_not_green` as the probability of the ball not landing in a green pocket
+p_not_green <- 1-p_green
+
+# Compute the standard error of the random variable.names
+abs((17 - -1))*sqrt(p_green*p_not_green)
+
+# Assign a variable `p_green` as the probability of the ball landing in a green pocket
+p_green <- green / (green+black+red)
+total<- (green+black+red)
+total 
+# Assign a variable `p_not_green` as the probability of the ball not landing in a green pocket
+p_not_green <- 1-p_green
+
+# Use the `set.seed` function to make sure your answer matches the expected result after random sampling
+set.seed(1)
+
+# Define the number of bets using the variable 'n'
+n <- 1000
+
+# Create a vector called 'X' that contains the outcomes of 1000 samples
+X <- sample(c(17,-1,-1), n, replace = TRUE, prob = c(2/38, 18/38, 18/38))  
+
+# Assign the sum of all 1000 outcomes to the variable 'S'
+S <- sum(X)
+
+# Print the value of 'S' to the console
+S
+
+# Define the number of bets using the variable 'n'
+n <- 1000
+
+# Calculate the expected outcome of 1,000 spins if you win $17 when the ball lands on green and you lose $1 when the ball doesn't land on green
+(-1*p_not_green+17*p_green)*1000
+
+
+# Compute the standard error of the sum of 1,000 outcomes
+abs((17 - -1))*sqrt(p_green*p_not_green)*sqrt(1000)
+
+# Assign a variable `p_green` as the probability of the ball landing in a green pocket
+p_green <- 2 / 38
+
+# Assign a variable `p_not_green` as the probability of the ball not landing in a green pocket
+p_not_green <- 1-p_green
+
+# Define the number of bets using the variable 'n'
+n <- 100
+
+# Calculate 'avg', the expected outcome of 100 spins if you win $17 when the ball lands on green and you lose $1 when the ball doesn't land on green
+avg <- n * (17*p_green + -1*p_not_green)
+
+# Compute 'se', the standard error of the sum of 100 outcomes
+se <- sqrt(n) * (17 - -1)*sqrt(p_green*p_not_green)
+
+# Using the expected value 'avg' and standard error 'se', compute the probability that you win money betting on green 100 times.
+1-pnorm(0, avg, se)
+
+
+
+
 
